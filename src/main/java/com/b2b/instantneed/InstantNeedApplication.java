dd8457544af9e1aec87f92b65e7983851a609a6d
suvraneel <bsuvraneel@gmail.com>
2026-05-23 18:02:07 +0530

    init

---
+++ src/main/java/com/b2b/instantneed/InstantNeedApplication.java
@@ -0,0 +1,13 @@
+package com.b2b.instantneed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantNeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstantNeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/b2b/instantneed/InstantNeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2b.instantneed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantNeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
